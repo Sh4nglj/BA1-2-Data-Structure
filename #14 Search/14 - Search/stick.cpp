59837493f376a9ac0f50366a38b1ea4243e5b720
Sh4nglj <sh4nglj@163.com>
2020-05-15 21:49:19 +0800

    new stick

--- #14 Search/14 - Search/stick.cpp
+++ #14 Search/14 - Search/stick.cpp
@@ -3,41 +3,38 @@
 #include <stdlib.h>
 using namespace std;
 
-inline bool cmpr(int a, int b, int c)
-{
-    if (a + b > c) {
-        return true;
-    }
-    else 
-        return false;
-}
-
 int main()
 {
     int n;
-    cin >> n;
-
-    int* arr = new int[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    while (cin >> n) {
+        //输入-1退出程序
+        if (n == -1)
+            break;
+        
+        int* arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        sort(arr, arr + n);
+            
+        int result = 0; 
+        for (int i = n - 1; i > 1; i--) {
+            int high = arr[i];
+            int mid = i - 1;
+            int low = 0;
+            while (mid > low) {
+                if (arr[low] + arr[mid] > high) {
+                    result += mid-- - low;
+                }
+                else {
+                    low++;
+                }//else
+            }//while
+        }//for
+        
+        cout << result << endl;
+        delete[] arr;
     }
-    sort(arr, arr + n);
-
-    int result = 0;
-    for (int i = 0; i < (n - 2); i++) {
-        int a = arr[i];
-        for (int j = i + 1; j < (n - 1); j++) {
-            int b = arr[j];
-            for (int k = j + 1; k < n; k++) {
-                int c = arr[k];
-                if (cmpr(a, b, c))
-                    result++;
-            }//for k
-        }//for j
-    }//for i
     
-    cout << result << endl;
-    delete[] arr;
-    system("pause");
-    return 0;    
+   return 0;
 }
